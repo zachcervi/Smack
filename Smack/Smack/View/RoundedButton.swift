dd5ad94727083e40454ba663815ff9e8f56fdb31
Zach Cervi <zachcervi@gmail.com>
2018-10-11 21:50:25 -0600

    Finished registering user and tested. Also enhanced UI with rounded buttons

---
+++ Smack/Smack/View/RoundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Zach Cervi on 10/11/18.
+//  Copyright © 2018 Zach Cervi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    func setupView () {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+}
