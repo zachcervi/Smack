e2bc417f13d9ec0233e58452c6111cc869028856
Zach Cervi <zachcervi@gmail.com>
2018-10-17 21:33:18 -0600

    finished Avatar picker. When the user selects the avatar it will set the profile image to that avatar

--- Smack/Smack/View/AvatarCell.swift
+++ Smack/Smack/View/AvatarCell.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+enum AvatarType{
+    case dark
+    case light
+}
 class AvatarCell: UICollectionViewCell {
     
     @IBOutlet weak var avatarImage: UIImageView!
@@ -17,10 +21,20 @@ class AvatarCell: UICollectionViewCell {
         setupView()
         
     }
+    func configureCell(index: Int, type: AvatarType){
+        if (type == AvatarType.dark){
+        avatarImage.image = UIImage(named: "dark\(index)")
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        }else{
+            avatarImage.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
+    }
     
     func setupView() {
         self.layer.backgroundColor = UIColor.lightGray.cgColor
         self.layer.cornerRadius = 10
         self.clipsToBounds = true
     }
+    
 }
